b22c8d89a2e33fa6ae462c76f47aafb997de514d
Quanhxoan25 <23020781@vnu.edu.vn>
2025-08-09 13:28:58 +0700

    Update FileStaffCSV.java

--- src/main/java/com/example/libraryoop/file_handle/FileStaffCSV.java
+++ src/main/java/com/example/libraryoop/file_handle/FileStaffCSV.java
@@ -8,14 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 import com.example.libraryoop.model.Staff;
 
 public class FileStaffCSV {
-    //
+
     private static final Path EXTERNAL_PATH = Paths.get("data", "StaffData.csv");
     private static final String HEADER = "ID || NAME || ADDRESS || EMAIL || PHONENUMBER || ROLE || USERNAME || PASSWORD";
 
@@ -29,7 +28,7 @@ public class FileStaffCSV {
             try (BufferedWriter bw = Files.newBufferedWriter(EXTERNAL_PATH,
                     StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
 
-                if (Files.notExists(EXTERNAL_PATH)) {
+                if (Files.size(EXTERNAL_PATH) == 0) {
                     bw.write(HEADER);
                     bw.newLine();
                 }
@@ -44,24 +43,62 @@ public class FileStaffCSV {
                     sj.add(nullToEmpty(staff.getNumberPhoneStaff()));
                     sj.add(nullToEmpty(staff.getRole()));
                     sj.add(nullToEmpty(staff.getUsername()));
-                    sj.add(nullToEmpty(staff.getPassWord()))
+                    sj.add(nullToEmpty(staff.getPassWord()));
 
                     bw.write(sj.toString());
                     bw.newLine();
                 }
             }
+
         } catch (IOException e) {
             throw new RuntimeException("Error writing ReaderData.csv: " + e.getMessage(), e);
         }
     }
     
-    public void readStaffFIle() {
+    public void readStaffFile() {
         try {
             if (Files.notExists(EXTERNAL_PATH.getParent())) {
                 Files.createDirectories(EXTERNAL_PATH.getParent());
             }
 
-            if (Files.notExists(EXTERNAL_PATH))
+            if (Files.notExists(EXTERNAL_PATH)) {
+                try (BufferedWriter writer =  Files.newBufferedWriter(EXTERNAL_PATH, 
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE)) {
+                    writer.write(HEADER);
+                    writer.newLine();
+                }
+            }
+
+            try (BufferedReader reader = Files.newBufferedReader(EXTERNAL_PATH, StandardCharsets.UTF_8)) {
+                String line;
+                List<Staff> staffs = new ArrayList<>();
+                boolean isFirstLine = true;
+
+                while ((line = reader.readLine()) != null) {
+                    if (isFirstLine) {
+                        isFirstLine = false;
+                        continue;
+                    }
+                    
+                    line.trim();
+
+                    String[] parts = line.split (" \\|\\| ");
+
+                    String id = parts.length > 0 ? emptyToNull(parts[0].trim()) : "";
+                    String name = parts.length > 1 ? emptyToNull(parts[1].trim()) : "";
+                    String address = parts.length > 2 ? emptyToNull(parts[2].trim()) : "";
+                    String email = parts.length > 3 ? emptyToNull(parts[3].trim()) : "";
+                    String phonenumber = parts.length > 4 ? emptyToNull(parts[4].trim()) : "";
+                    String role = parts.length > 5 ? emptyToNull(parts[5].trim()) : "";
+                    String username = parts.length > 6 ? emptyToNull(parts[6].trim()) : "";
+                    String password = parts.length > 7 ? emptyToNull(parts[7].trim()) : "";
+
+                    Staff staff = new Staff(id, name, email, phonenumber, address, role, username, password);
+                    staffs.add(staff);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Loi khi xu ly file" + e.getMessage());
         }
     }
 
