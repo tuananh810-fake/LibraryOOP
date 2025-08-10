32e19b8b6a471b04508822f531a01ce0dc95d780
Quanhxoan25 <23020781@vnu.edu.vn>
2025-08-10 15:41:30 +0700

    Update StaffManagementService.java

--- src/main/java/com/example/libraryoop/service/StaffManagementService.java
+++ src/main/java/com/example/libraryoop/service/StaffManagementService.java
@@ -1,14 +1,15 @@
 package com.example.libraryoop.service;
 
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
 import com.example.libraryoop.file_handle.FileStaffCSV;
 import com.example.libraryoop.model.Staff;
-import com.example.libraryoop.util.IdGenerator;
+import java.util.Iterator;
+import java.util.Map;
 
 public class StaffManagementService {
     private static final List<Staff> staffList = new ArrayList<>();
+    private static int idStaff = 0;
 
     public void addStaff(Staff staff) {
         if (staff == null) {
@@ -24,21 +25,111 @@ public class StaffManagementService {
             throw new IllegalArgumentException("Illegal Argument");
         }
 
-        String newId;
-        
         do {
-            newId = IdGenerator.generateId("S");
-        } while (getReaderById(newId) != null);
+            idStaff++;
+        } while (!checkingStaffId(String.valueOf(idStaff)));
         
-        String name = staff.getNameStaff();
-        String address = staff.getAddressStaff();
-        String email = staff.getEmailStaff();
-        String numberPhone = staff.getNumberPhoneStaff();
-        String role = staff.getRole();
-        String username = staff.getUsername();
-        String password = staff.getPassWord();
+        staff.setIdStaff(String.valueOf(idStaff));
+        addStaff(staff);
 
-        Staff newStaff = new Staff(newId, name, address, email, numberPhone, role, username, password);
+    }
 
+    public void update(String id, Map<String, Object> fieldsToUpdate) throws Exception {
+        int index = findIndexStaff(id);
+        if (index == -1) {
+            throw new Exception("Không tìm thấy độc giả để cập nhật!");
+        }
+        
+        Staff existingStaff = staffList.get(index);
+        
+        // Cập nhật từng trường thông tin
+        for (Map.Entry<String, Object> entry : fieldsToUpdate.entrySet()) {
+            String fieldName = entry.getKey();
+            Object value = entry.getValue();
+            
+            switch (fieldName) {
+                case "nameStaff":
+                    String newName = (String) value;
+                    if (newName == null || newName.trim().isEmpty()) {
+                        throw new Exception("Tên độc giả không được để trống.");
+                    }
+                    existingStaff.setNameStaff(newName);
+                    break;
+                    
+                case "addressReader":
+                    existingStaff.setAddressStaff((String) value);
+                    break;
+                    
+                case "emailReader":
+                    String newEmail = (String) value;
+                    if (!newEmail.isEmpty()) {
+                        throw new Exception("Email không hợp lệ.");
+                    }
+                    existingStaff.setEmailStaff(newEmail);
+                    break;
+                    
+                case "phoneNumber":
+                    String newPhone = (String) value;
+                    if (!newPhone.isEmpty()) {
+                        throw new Exception("Số điện thoại không hợp lệ.");
+                    }
+                    existingStaff.setNumberPhoneStaff(newPhone);
+                    break;
+                case "role":
+                    String newRole = (String) value;
+                    existingStaff.setRole(newRole);
+                    break;
+                    
+                case "username":
+                    String username = (String) value;
+                    existingStaff.setUsername(username);
+                    break;
+                case "password":
+                    String password = (String) value;
+                    existingStaff.setPassWord(password);
+                default:
+                    System.out.println("Cảnh báo: Trường '" + fieldName + "' không hợp lệ.");
+                    break;
+            }
+        }
+        
+        FileStaffCSV.writeStaffFile(staffList);
+    }
+
+    public void deleteStaff(String id) throws Exception {
+        if (checkingStaffId(id) == false) {
+            throw new Exception("Khong tim thay staff");
+        }
+        Iterator<Staff> iterator = staffList.iterator();
+        
+        while (iterator.hasNext()) {
+            
+            Staff staff = iterator.next();
+
+            if (staff.getIdStaff().equals(id)) {
+                iterator.remove();
+                System.out.println("Xoa staff thanh cong");
+            }
+        }
+    }
+
+    public int findIndexStaff(String id) {
+        for (int i = 0; i < staffList.size(); i++) {
+            if (staffList.get(i).getIdStaff().equals(id)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean checkingStaffId(String id) {
+        for (Staff staff : staffList) {
+            if (staff.getIdStaff() == id) {
+                return true; 
+            }
+        }
+
+        return false;
     }
 }
