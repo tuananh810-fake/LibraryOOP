6c7bd3acffbdce7c637505cd8e582bef48d67317
Quanhxoan25 <23020781@vnu.edu.vn>
2025-08-12 14:35:44 +0700

    Update Service

--- src/main/java/com/example/libraryoop/service/CallCardManagementService.java
+++ src/main/java/com/example/libraryoop/service/CallCardManagementService.java
@@ -1,4 +1,30 @@
 package com.example.libraryoop.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.example.libraryoop.model.CallCard;
+import com.example.libraryoop.file_handle.CallCardCSV;
+
 public class CallCardManagementService {
+    private static List<CallCard> callCardList = new ArrayList<>();
+
+    public void addCallCard(CallCard callCard) {
+        if (callCard == null) {
+            throw new IllegalArgumentException("Illegal Argument");
+        } 
+
+        callCardList.add(callCard);
+        CallCardCSV.writeCallCardListToFile(callCardList);
+    }
+
+    public void addNewCallCard(CallCard callCard) {
+        if (callCard == null) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+
+        String newID;
+        
+        do
+
+    }
 }
